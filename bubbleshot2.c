e3f5eb1b7b6edf8436ee4b18a14e5afbd9002c46
priyanshusuman <54205429+priyanshusuman@users.noreply.github.com>
2019-10-21 09:53:37 +0530

    Create bubbleshot2.c

---
+++ bubbleshot2.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+swap( int a[], int b)
+{
+	int i,j,temp;
+	for(i=0;i<b-1;i++)
+	{
+		for(j=0;j<b-i-1;j++)
+		{
+			if(a[j]>a[j+1])
+	{
+			temp=a[j];
+		a[j]=a[j+1];
+		a[j+1]=temp;}	
+		}
+	}
+}
+
+
+
+
+int main()
+{
+	int n,a[n],i;
+	printf("enter the no. of rows");
+	scanf("%d",&n);
+	printf("enter %d value ",n);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	swap(a,n);
+	for(i=0;i<n;i++)
+	{
+		printf("%d",a[i]);
+	}
+}
